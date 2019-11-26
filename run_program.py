3e0da21b59cb735c4e243c6941f79eeefe93cdff
Hillary Cederna <hcederna@gmail.com>
2019-11-26 11:44:15 -0800

    Update run program script to handle elastic constraint ensuring solution uniqueness

--- run_program.py
+++ run_program.py
@@ -15,14 +15,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Solve linear program to assign students to groups subject to constraints.")
     parser.add_argument("filename", type=str, help="filename for Excel spreadsheet with student, group, and constraint data as your_filename_here.xlsx")
     parser.add_argument("num_solutions", type=int, help="desired number of optimal solutions")
-    parser.add_argument("--pct_changed", type=float, help="desired percent (as a decimal) of student group assignments changed per optimal solution")
     parser.add_argument("-s", "--save", action="store_true", help="save student groups to Excel spreadsheet")
     parser.add_argument("-v", "--verbose", action="store_true", help="print solution(s) in terminal")
     args = parser.parse_args()
 
     filename = "data/" + args.filename
-
-    num_unchanged = calculate_num_unchanged(args.pct_changed, filename)
     unique_solutions = []
 
 
@@ -34,8 +31,7 @@ if __name__ == "__main__":
         print("Solving with rigid maximum constraint...")
         status, solution_df = run_lp_problem(filename,
                                              elastic=False,
-                                             unique_solutions=unique_solutions,
-                                             num_unchanged=num_unchanged)
+                                             unique_solutions=unique_solutions)
 
         if status != "Optimal":
 
@@ -44,12 +40,11 @@ if __name__ == "__main__":
             print("Solving with elastic maximum constraint...")
             status, solution_df = run_lp_problem(filename,
                                                  elastic=True,
-                                                 unique_solutions=unique_solutions,
-                                                 num_unchanged=num_unchanged)
+                                                 unique_solutions=unique_solutions)
 
             if status != "Optimal":
 
-                print("Unsolvable with rigid or elastic constraint. Solution "+status)
+                print(f"Unsolvable with rigid or elastic constraint. Solution {status}.")
                 break
 
         print(f"{status}...\n")
